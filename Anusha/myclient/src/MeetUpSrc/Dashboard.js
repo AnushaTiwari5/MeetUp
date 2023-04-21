61686fdcd4d6e571320e1b4cdab1b04cd0372e3e
HQtom <1662560454@qq.com>
2023-04-21 18:36:55 -0400

    update

--- Anusha/myclient/src/MeetUpSrc/Dashboard.js
+++ Anusha/myclient/src/MeetUpSrc/Dashboard.js
@@ -9,14 +9,22 @@ import { Space, Table, Tag } from 'antd';
 import { useContext } from "react";
 import MyNavbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
-
+import { Descriptions } from 'antd';
 
 export default function Dashboard() {
-
+    const [eventID, setEventID] = useState(0);
     const { currentUser } = useContext(AuthContext);
     const [events, setEvents] = useState(undefined);
     const userCredential = firebase.auth().currentUser;
+    const [detail,setDetails] = useState({})
     const navigate = useNavigate();
+    useEffect(() => {
+        fetch(`http://localhost:3000/getInvite`)
+            .then((res) => res.json())
+            .then((res) => {
+                setEventID(res);
+            })
+    }, [])
 
     useEffect(() => {
         const getEvents = async () => {
@@ -35,12 +43,14 @@ export default function Dashboard() {
 
     }, []);
 
-    const click =()=>{
-
+    const click =(record)=>{
+        
+        setDetails(record);
     }
 
-    const click2 =()=>{
-        navigate('/Invite')
+    const click2 =(record)=>{
+        console.log(record);
+        alert(`Your invitation code is: http://localhost:3000/Response/`+record.event_id)
     }
 
     const onChange = (pagination, filters, sorter, extra) => {
@@ -52,10 +62,7 @@ export default function Dashboard() {
             title: 'title',
             dataIndex: 'title'
         },
-        {
-            title: 'description',
-            dataIndex: 'description'
-        },
+       ,
         {
             title:  'Time',
             dataIndex: 'final_time'
@@ -88,7 +95,26 @@ export default function Dashboard() {
 
             <Table columns={columns} dataSource={events} onChange={onChange} />
 
-        </div></div>
+        </div>
+        <p>
+        <div>
+            <Descriptions
+              title="Event Descriptions"
+              bordered
+              column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+            >
+              <Descriptions.Item label="Title">{detail.title}</Descriptions.Item>
+              <Descriptions.Item label="Orgnizer">{detail.orgnizer}</Descriptions.Item>
+              <Descriptions.Item label="time">{detail.final_time}</Descriptions.Item>
+              <Descriptions.Item label="Location">{detail.final_location}</Descriptions.Item>
+              <Descriptions.Item label="Event-ID">{detail.event_id}</Descriptions.Item>
+              <Descriptions.Item label="Description">
+              {detail.description}
+              </Descriptions.Item>
+            </Descriptions>
+          </div>
+        </p>
+        </div>
     
     )
 }
